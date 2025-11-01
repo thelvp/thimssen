7ae6f55eaf0c489c803a2f2bf110a40c4e538a05
Loes van Puijenbroek <loesvanpuijenbroek@gmail.com>
2025-11-01 12:05:22 +0100

    update footer styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,33 +2,38 @@ import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const Footer = () => {
+  const baseLinkClasses = 'me-4 md:me-6 font-bold';
+
   return (
-    <footer className="fixed bottom-0 left-0 z-20 flex w-full justify-between p-6 text-gray-400">
-      <ul className="mt-3 flex flex-wrap items-center text-sm font-bold sm:mt-0">
+    <footer className="fixed bottom-0 left-0 z-20 flex w-full justify-between p-8 text-gray-300">
+      <ul className="mt-3 flex flex-wrap items-center text-sm sm:mt-0">
         <li>
-          <a href="#" className="me-4 hover:text-blue-500 md:me-6">
+          <a href="/" className={`${baseLinkClasses} hover:text-blue-500`}>
             Home
           </a>
         </li>
         <li>
-          <a href="#" className="me-4 hover:text-purple-500 md:me-6">
+          <a href="#" className={`${baseLinkClasses} hover:text-purple-500`}>
             About
           </a>
         </li>
         <li>
-          <a href="#" className="me-4 hover:text-pink-500 md:me-6">
+          <a href="#" className={`${baseLinkClasses} hover:text-pink-500`}>
             Work
           </a>
         </li>
         <li>
-          <a href="#" className="me-4 hover:text-orange-500 md:me-6">
+          <a href="#" className={`${baseLinkClasses} hover:text-orange-500`}>
             Contact
           </a>
         </li>
         <li>
           <a
+            target="_blank"
+            rel="noopener noreferrer"
             href="https://www.instagram.com/thimssen/"
-            className="hover:font-extrabold hover:text-yellow-500 md:me-6"
+            className={`${baseLinkClasses} hover:font-extrabold hover:text-yellow-500`}
+            aria-label="Instagram"
           >
             <FontAwesomeIcon icon={faInstagram} />
           </a>
