c15cbc3e7e86cbd8880cf121d9626e3e3c4d5a3a
Loes van Puijenbroek <loesvanpuijenbroek@gmail.com>
2025-11-02 09:11:11 +0100

    Remove weird padding on top of content

--- src/components/ContentLayout.tsx
+++ src/components/ContentLayout.tsx
@@ -2,7 +2,7 @@ import type { PropsWithChildren } from 'react';
 
 export const ContentLayout = ({ children }: PropsWithChildren<{}>) => {
   return (
-    <div className="container flex h-screen w-full flex-col items-center justify-center text-center md:flex-row">
+    <div className="container flex h-screen w-full flex-col justify-center text-center md:flex-row">
       {children}
     </div>
   );
