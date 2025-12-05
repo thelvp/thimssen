23f77e15ce38fba7b12eeb402500aa24961ed586
Loes van Puijenbroek <loesvanpuijenbroek@gmail.com>
2025-12-05 13:31:15 +0100

    Always show logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,18 +1,13 @@
 import { faCompactDisc } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useIsMobile } from '../../hooks/useIsMobile';
 
 export const Logo = () => {
-  const isMobile = useIsMobile();
-
   return (
     <div className="flex text-xl font-bold sm:text-3xl">
       <FontAwesomeIcon icon={faCompactDisc} className="pr-2 text-blue-600" />
-      {!isMobile && (
-        <h1 className="bg-[linear-gradient(to_right,var(--color-blue-600),var(--color-purple-500),var(--color-pink-400),var(--color-orange-300))] bg-clip-text text-transparent">
-          Thimssen
-        </h1>
-      )}
+      <h1 className="bg-[linear-gradient(to_right,var(--color-blue-600),var(--color-purple-500),var(--color-pink-400),var(--color-orange-300))] bg-clip-text text-transparent">
+        Thimssen
+      </h1>
     </div>
   );
 };
