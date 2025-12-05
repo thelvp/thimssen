e558d8901a69d4f83fcaaa3230f1c115ebb65110
Loes van Puijenbroek <loesvanpuijenbroek@gmail.com>
2025-12-05 14:48:56 +0100

    Style updates portfolio item

--- src/components/PortfolioItem/PortfolioItem.tsx
+++ src/components/PortfolioItem/PortfolioItem.tsx
@@ -23,7 +23,7 @@ export const PortfolioItem = ({
       // href={href}
       // target="_blank"
       // rel="noopener noreferrer"
-      className="m-3 transform cursor-pointer overflow-hidden rounded-2xl bg-white text-left text-black/90 transition-transform duration-200 ease-out hover:scale-95"
+      className="hover:scale-99 m-3 transform cursor-pointer overflow-hidden rounded-2xl bg-white text-left text-black/90 transition-transform duration-200 ease-out"
     >
       <div>
         <img
@@ -33,18 +33,18 @@ export const PortfolioItem = ({
         />
       </div>
 
-      <div className="flex flex-col justify-center border-b-4 border-b-black p-3 font-extrabold">
+      <div className="flex flex-col justify-center p-3 font-extrabold">
         <h3 className="text-xl">{artistName}</h3>
         <h3 className="text-3xl"> {title}</h3>
       </div>
 
-      <div className="flex items-center justify-between p-3">
-        <p className="mr-8 text-base">{year}</p>
+      <div className="flex items-center justify-between border-t-2 border-black/20 p-3">
+        <p className="mr-8 text-base font-bold">{year}</p>
         <ul className="flex flex-wrap">
           {categoryItems.map((item) => {
             return (
-              <li key={item} className="m-1">
-                <p className="rounded-2xl bg-amber-400 px-2 py-1 font-mono text-[10px] sm:px-3 sm:py-2 sm:text-xs">
+              <li key={item} className="flex justify-evenly">
+                <p className="m-0.5 rounded-2xl bg-amber-400/75 px-2 py-1 font-mono text-sm text-black">
                   {item}
                 </p>
               </li>
