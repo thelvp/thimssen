339d5d1d6cf273d7b1277b36607972bbf5e73375
Loes van Puijenbroek <loesvanpuijenbroek@gmail.com>
2025-12-05 15:21:43 +0100

    PortfolioItem overlay

--- src/components/PortfolioItem/PortfolioItem.tsx
+++ src/components/PortfolioItem/PortfolioItem.tsx
@@ -1,3 +1,12 @@
+import {
+  faInstagram,
+  faSpotify,
+  faYoutube,
+} from '@fortawesome/free-brands-svg-icons';
+import { faLink } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState } from 'react';
+
 type MediaType = 'Instagram' | 'Youtube' | 'Spotify' | 'Other';
 
 export interface PortfolioItemProps {
@@ -14,16 +23,61 @@ export const PortfolioItem = ({
   title,
   year,
   categoryItems,
-  // TODO:
   links,
   imageSrc,
 }: PortfolioItemProps) => {
+  const [clickedOpen, setClickedOpen] = useState(false);
+  const [hoverOpen, setHoverOpen] = useState(false);
+  const modalOpen = clickedOpen || hoverOpen;
+
+  const onHandleClick = () => {
+    setClickedOpen((state) => !state);
+  };
+
+  const getIcon = (mediaType: MediaType) => {
+    switch (mediaType) {
+      case 'Instagram':
+        return <FontAwesomeIcon icon={faInstagram} />;
+      case 'Spotify':
+        return <FontAwesomeIcon icon={faSpotify} />;
+      case 'Youtube':
+        return <FontAwesomeIcon icon={faYoutube} />;
+      case 'Other':
+        return <FontAwesomeIcon icon={faLink} />;
+      default:
+        return <FontAwesomeIcon icon={faLink} />;
+    }
+  };
+
+  const getIconColor = (mediaType: MediaType) => {
+    switch (mediaType) {
+      case 'Instagram':
+        return 'pink-700';
+      case 'Spotify':
+        return 'green-500';
+      case 'Youtube':
+        return 'red-500';
+      case 'Other':
+        return 'blue-500';
+      default:
+        return 'blue-500';
+    }
+  };
+
   return (
-    <div
-      // href={href}
-      // target="_blank"
-      // rel="noopener noreferrer"
-      className="hover:scale-99 m-3 transform cursor-pointer overflow-hidden rounded-2xl bg-white text-left text-black/90 transition-transform duration-200 ease-out"
+    <article
+      tabIndex={0}
+      onKeyDown={(e) => {
+        if (e.key === 'Enter' || e.key === ' ') {
+          e.preventDefault();
+          onHandleClick();
+        }
+      }}
+      onClick={onHandleClick}
+      onMouseEnter={() => setHoverOpen(true)}
+      onMouseLeave={() => setHoverOpen(false)}
+      aria-expanded={modalOpen}
+      className="relative m-3 transform cursor-pointer overflow-hidden rounded-2xl bg-white text-left text-black/90 transition-transform duration-200 ease-out"
     >
       <div>
         <img
@@ -52,6 +106,49 @@ export const PortfolioItem = ({
           })}
         </ul>
       </div>
-    </div>
+
+      {modalOpen && (
+        <div
+          role="dialog"
+          aria-hidden={!modalOpen}
+          className="absolute inset-0 z-50 flex flex-col items-center justify-center overflow-auto bg-black/80 p-6"
+          onClick={(e) => {
+            e.stopPropagation();
+            if (e.target === e.currentTarget) {
+              setClickedOpen(false);
+              setHoverOpen(false);
+            }
+          }}
+        >
+          <p className="mb-6 text-center text-2xl text-white">
+            Check het hier:
+          </p>
+          <ul className="not-last:mr-1 flex">
+            {links.map((link) => (
+              <li key={link.url} className="mb-4 text-6xl">
+                <a
+                  href={link.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    setClickedOpen(false);
+                    setHoverOpen(false);
+                  }}
+                  className="inline-flex items-center gap-3"
+                >
+                  <span
+                    aria-hidden="true"
+                    className={`text-white hover:text-${getIconColor(link.mediaType)} `}
+                  >
+                    {getIcon(link.mediaType)}
+                  </span>
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </article>
   );
 };
