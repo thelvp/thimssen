54f7e86cd95792156b420fd08e6035f7dd69c031
Loes van Puijenbroek <loesvanpuijenbroek@gmail.com>
2026-01-25 15:21:02 +0100

    Make credits in footer more subtle

--- src/components/Footer/FooterCreds.tsx
+++ src/components/Footer/FooterCreds.tsx
@@ -1,14 +1,13 @@
 export const FooterCreds = () => {
   return (
-    <p className="items-end text-xs text-gray-500 hover:text-gray-300">
+    <p className="text-xs text-gray-500">
       <a
-        className={'group hover:font-semibold'}
         href="https://github.com/thelvp"
         target="_blank"
         rel="noopener noreferrer"
       >
         Designed with{' '}
-        <span aria-hidden="true" className="group-hover:text-red-400">
+        <span aria-hidden="true" className="text-red-400">
           ♡
         </span>{' '}
         by thelvp
