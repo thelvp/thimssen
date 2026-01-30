2aa109658a3da2b7bb60f4be6fd77c89b451f810
Loes van Puijenbroek <loesvanpuijenbroek@gmail.com>
2026-01-30 19:50:42 +0100

    add todo

--- src/data/NAV_ITEMS.ts
+++ src/data/NAV_ITEMS.ts
@@ -7,6 +7,7 @@ export const NAV_ITEMS: NavItemProps[] = [
   { href: '/', title: 'Home' },
   { href: '/about', title: 'Over' },
   { href: '/projects', title: 'Projecten' },
+  // TODO: uncomment once this page can be added to navbar
   // { href: '/music', title: 'Muziek' },
   { href: '/contact', title: 'Contact' },
 ];
