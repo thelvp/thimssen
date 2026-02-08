8b50b72d18988192525fe7980c839ee80b3e20c7
Loes van Puijenbroek <loesvanpuijenbroek@gmail.com>
2026-02-08 16:09:37 +0100

    Make navbar sticky for mobile, adjust grid slightly for responsiveness Producing & Music page

--- src/pages/Producing.tsx
+++ src/pages/Producing.tsx
@@ -4,7 +4,7 @@ import { PortfolioItem } from '../components/PortfolioItem/PortfolioItem';
 export function Producing() {
   return (
     <div className="w-full sm:mt-3 md:w-[90%]">
-      <div className="grid w-full grid-cols-1 place-items-stretch sm:grid-cols-3 sm:gap-4 md:grid-cols-4 md:gap-6">
+      <div className="grid w-full grid-cols-1 place-items-stretch sm:grid-cols-2 sm:gap-2 md:grid-cols-3 md:gap-6 lg:grid-cols-4">
         {PORTFOLIO_ITEMS.map((item) => {
           const idBase = `${item.artistName}-${item.title}`
             .replace(/\s+/g, '-')
