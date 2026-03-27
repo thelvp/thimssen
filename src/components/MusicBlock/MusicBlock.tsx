8353664df4bae23547bd1ccb8dca1cafc3da8f33
Loes van Puijenbroek <loesvanpuijenbroek@gmail.com>
2026-03-27 18:12:32 +0100

    Updatd spotify playlist

--- src/components/MusicBlock/MusicBlock.tsx
+++ src/components/MusicBlock/MusicBlock.tsx
@@ -4,11 +4,11 @@ export const MusicBlock = () => {
       <iframe
         data-testid="embed-iframe"
         // style="border-radius:12px"
-        src="https://open.spotify.com/embed/album/1mF78ngA3JVHrG3sq39frl?utm_source=generator&theme=0"
+        src="https://open.spotify.com/embed/playlist/4RWUaXb6YTrGqxUqktz2wv?utm_source=generator&theme=0"
         width="100%"
         height="352"
         frameBorder="0"
-        // allowfullscreen={true}
+        //allowfullscreen=""
         allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
         loading="lazy"
       ></iframe>
